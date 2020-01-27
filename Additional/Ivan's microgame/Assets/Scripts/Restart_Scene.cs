ea1fb7495fe9caeb34b3d08a8721df235328665a
ROB1N2401 <60138872+ROB1N2401@users.noreply.github.com>
2020-01-27 23:08:49 +0100

    Changes added by Ivan
    -Created "additional" folder;
    -Added the assets from my microgame (the team's previous Unity project)

---
+++ Additional/Ivan's microgame/Assets/Scripts/Restart_Scene.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart_Scene : MonoBehaviour
+{
+    private Animation explosion;
+    bool animation_is_played;
+    string name_;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        name_ = GetComponent<Animation>().name;
+        explosion = GetComponent<Animation>();
+        if (explosion == null)
+        {
+            Debug.Log("Error: Did not find animation!");
+        }
+        else
+        {
+            //Debug.Log("Got anim");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //animation_is_played = GetComponent<Animation>().IsPlaying(name_);
+        //if (!animation_is_played)
+            StartCoroutine(RestartGame());
+    }
+
+    IEnumerator RestartGame()
+    {
+        yield return new WaitForSeconds(4.0f);
+        SceneManager.LoadScene("SampleScene");
+    }
+}

261001827d32d3fba72742922effffb9ec36c563
Dan <44238640+grommithub@users.noreply.github.com>
2020-01-29 19:38:51 +0100

    Revert "Revert "Changes added by Ivan""
    This reverts commit b853f84ea809546153d83234a3a500a7cc16b395.

---
+++ Additional/Ivan's microgame/Assets/Scripts/Fade_Out.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+class Fade_Out : MonoBehaviour
+{
+    //Fade time in seconds
+    public float fadeOutTime;
+    void Start()
+    {
+        StartCoroutine(FadeOutRoutine());
+    }
+
+    private IEnumerator FadeOutRoutine()
+    {
+        TextMeshProUGUI text = GetComponent<TextMeshProUGUI>();
+        Color originalColor = text.color;
+        for (float t = 0.01f; t < fadeOutTime; t += Time.deltaTime)
+        {
+            text.color = Color.Lerp(originalColor, Color.clear, Mathf.Min(1, t / fadeOutTime));
+            yield return null;
+        }
+    }
+}
+

6dff69e0099a2a1b5cf3f904e411243de1145b05
Dan <44238640+grommithub@users.noreply.github.com>
2020-01-30 15:44:08 +0100

    fix everything

---
+++ Shmop/Assets/Scripts/Weapon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public float _shootSpeed;
+    public GameObject projectile;
+    
+
+    public virtual void Shoot(Vector2 position, Quaternion rotation, bool enemy, bool goingRight)
+    {
+        GameObject proj = Instantiate(projectile, position, Quaternion.identity);
+        proj.transform.up = Quaternion.AngleAxis(-90f + rotation.eulerAngles.z, Vector3.forward) * Vector3.up;
+
+        ProjectileBase projectileBase = proj.GetComponent<ProjectileBase>();
+        if(projectileBase != null)
+        {
+            projectileBase.isEnemy = enemy;
+            projectileBase.goingRight = goingRight;
+        }
+    }
+}

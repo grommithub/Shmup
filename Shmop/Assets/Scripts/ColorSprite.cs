d287ede29a3f9c028b06cbb6d9f6c7a0801c4f6d
Dan <44238640+grommithub@users.noreply.github.com>
2020-02-06 15:09:01 +0100

    Changed folder, implemented color change

---
+++ Shmop/Assets/Scripts/ColorSprite.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorSprite : MonoBehaviour
+{
+    //[SerializeField] internal bool changingColour;
+
+    private SpriteRenderer _sprite;
+    private ColorController _controller;
+    private void Start()
+    {
+        _sprite = GetComponent<SpriteRenderer>();
+        _controller = GameObject.FindObjectOfType<ColorController>();
+
+        Update();
+    }
+    private void Update()
+    {
+        if(_sprite != null && _controller != null)
+        {
+            _sprite.color = _controller._colour;
+        }
+    }
+}

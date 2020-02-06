cbd3eba93c0bdb5bb7110fb844175358f710a4b2
Dan <44238640+grommithub@users.noreply.github.com>
2020-02-06 15:31:11 +0100

    Revert "Revert "Changed folder, implemented color change""
    This reverts commit 44d37597d4d3aee5435ac86fbc23cb8bae22e6c6.

---
+++ Shmop/Assets/ColorController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorController : MonoBehaviour
+{
+    public Color _colour;
+    [SerializeField] private Color baseColour = Color.white;
+    [SerializeField] private Color _temporaryColour;
+    [SerializeField] private float _colorChangeSpeed;
+    [SerializeField] private bool _snap;
+    private float _tempColourEndTime;
+
+    //stretch goal
+
+    //public void LerpColour(Color c, float sp)
+    //{
+      
+    //}
+    //public void LerpColour(Color c, float sp, float time)
+    //{
+       
+    //}
+    public void SetBaseColour(Color c)
+    {
+        baseColour = c;
+    }
+    public void SetTemporaryColor(Color c, float time)
+    {
+        _temporaryColour = c;
+        _tempColourEndTime = Time.time + time;
+    }
+    private void Update()
+    {
+        if(Time.time < _tempColourEndTime)
+        {
+            _colour = _temporaryColour;
+        }
+        else
+        {
+            _colour = baseColour;
+        }
+    }
+}

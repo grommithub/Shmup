07fce9de522c8397ac61efb5155a59660d712de4
grommithub <44238640+grommithub@users.noreply.github.com>
2020-02-16 00:37:41 +0100

    pushing a million things and hoping nothing breaks

--- Shmop/Assets/Scripts/Effects/Scrolling_Background.cs
+++ Shmop/Assets/Scripts/Effects/Scrolling_Background.cs
@@ -1,29 +1,29 @@
 ﻿//script source (tutorial): https://www.youtube.com/watch?v=32EIYs6Z18Q
 //Responsible for the script: Ivan
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Scrolling_Background : MonoBehaviour
 {
+    private ColorController colour_controller_;
+
     public float bg_speed_ = 0f; //speed of background scrolling
     public Renderer bg_renderer_; //put the mesh renderer here
-
+    
     [Header("Hyperdrive")]
-    private ColorController colour_controller_;
-    private bool hyperdriving_;
+
     [SerializeField] private float hyper_drived_speed_;
     [SerializeField] private float bg_stretch_;
-    [SerializeField] private float acceleration_;
 
-    public void SetSpeed(float speed)
+    private void Start()
     {
-        bg_speed_ = speed;
+        colour_controller_ = FindObjectOfType<ColorController>();
     }
-    public void SetStretch(float stretch)
+
+    public void SetHyperdriveValues()
     {
-        hyperdriving_ = true;
+        bg_speed_ = hyper_drived_speed_;
         bg_renderer_.material.mainTextureScale = new Vector2(bg_stretch_, 1f);
     }
 
@@ -31,11 +31,5 @@ public class Scrolling_Background : MonoBehaviour
     {
         bg_renderer_.material.mainTextureOffset += new Vector2(bg_speed_ * Time.deltaTime, 0f);
         bg_renderer_.material.color = colour_controller_._currentColour;
-        if (hyperdriving_)
-        {
-            bg_speed_ = Mathf.SmoothStep(bg_speed_, hyper_drived_speed_, acceleration_*  Time.deltaTime);
-            bg_renderer_.material.mainTextureScale = Vector2.Lerp(bg_renderer_.material.mainTextureScale, new Vector2(bg_stretch_, 1f), acceleration_ * Time.deltaTime);
-        }
     }
 }
-

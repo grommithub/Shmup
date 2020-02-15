6f571d459f23648f910044288bc6cd2c46114191
grommithub <44238640+grommithub@users.noreply.github.com>
2020-02-15 23:17:22 +0100

    aha

--- Shmop/Assets/Hyperdrive.cs
+++ Shmop/Assets/Hyperdrive.cs
@@ -6,43 +6,72 @@ using UnityEngine.Events;
 public class Hyperdrive : MonoBehaviour
 {
     [SerializeField] private UnityEvent _startHyperDrive;
-    private Vector3 _middle = new Vector3();
+    [SerializeField] private UnityEvent _startCentering;
+    
+    [SerializeField] private float _centeringTime = 5f;
+    [SerializeField] private float _hyperDrivespeed = 30f;
+    
     private bool _hyperDriving;
-
     private bool _centering;
+<<<<<<< Updated upstream
     private float _distance;
 
     [SerializeField] private float _hyperDrivespeed = 30f;
     [SerializeField] private float _goToMiddleSpeed = 3f;
     [SerializeField] private float _stretch = 1.25f;
+=======
+
+    private Vector2 _middle = new Vector2();
+    private Vector2 _startOffset = new Vector2();
+    
+    private float _centerStartTime;
+
+>>>>>>> Stashed changes
     private void Start()
     {
         _middle = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width/3, Screen.height/2, 0f));
-        _middle.z = 0f;
     }
 
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.H) && !_centering && !_hyperDriving)
         {
+<<<<<<< Updated upstream
+=======
+            _startOffset = (Vector2)transform.position - _middle;
+            _centerStartTime = Time.time;
+
+            _startCentering.Invoke();
+>>>>>>> Stashed changes
             _hyperDriving = true;
             _centering = true;
-             
         }
+        
         if (!_hyperDriving) return;
+
         if (_centering)
         {
-            if ((transform.position - _middle).magnitude < 0.05f)
+            if (Time.time >= _centerStartTime + _centeringTime)
             {
                 _centering = false;
                 _startHyperDrive.Invoke();
             }
-        transform.position = Vector3.Lerp(transform.position, _middle, _goToMiddleSpeed * Time.deltaTime);
+
+            float fraction = ((Time.time - _centerStartTime)  / _centeringTime);
+            fraction = Mathf.Min(1f, fraction);
+            transform.position = (_middle + _startOffset) - (_startOffset * fraction);
         }
         else
         {
+<<<<<<< Updated upstream
             transform.position += (Vector3.right * _hyperDrivespeed * Time.deltaTime);
             transform.localScale = new Vector3(_stretch, 1f, 1f);
+=======
+
+            transform.Translate(Vector3.right * _hyperDrivespeed * Time.deltaTime);
+            transform.localScale = new Vector3(2f, 1f, 1f);
+>>>>>>> Stashed changes
         }
+        transform.rotation = Quaternion.identity;
     }
 }
---
+++ Shmop/Assets/Scripts/GameManager/Waves.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waves : MonoBehaviour
+{
+    [SerializeField] private List<Wave> _waves = new List<Wave>();
+    private RandomSpawner _spawner;
+
+
+    private void Start()
+    {
+        _spawner = GetComponent<RandomSpawner>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            SpawnWave(0);
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            SpawnWave(1);
+    }
+
+    private void SpawnWave(int index)
+    {
+        if (index < 0) return;
+        index = Mathf.Min(index, _waves.Count - 1);
+        Wave w = _waves[(int)index];
+        _spawner.SpawnWave(w.enemy, w.amount, w.interval);
+    }
+}
+
+[System.Serializable]
+public class Wave
+{
+    public GameObject enemy;
+    public int amount;
+    public float interval;
+}

0d95da69f67be3cf8177a5f8a4370b887233a100
grommithub <44238640+grommithub@users.noreply.github.com>
2020-02-16 21:04:13 +0100

    bring back boundaries?

---
+++ Shmop/Assets/Scripts/Entities/Boundaries.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundaries : MonoBehaviour
+{
+    private Vector2 ScreenBoundaries_;
+    private float ObjectWidth_;
+    private float ObjectHeight_;
+
+
+    void Start()
+    {
+        ScreenBoundaries_ = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        ObjectWidth_ = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2;
+        ObjectHeight_ = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 ViewPos_ = transform.position;
+        //ViewPos_.x = Mathf.Clamp(ViewPos_.x, -ScreenBoundaries_.x + ObjectWidth_, ScreenBoundaries_.x - ObjectWidth_);
+        ViewPos_.y = Mathf.Clamp(ViewPos_.y, -ScreenBoundaries_.y + ObjectHeight_, ScreenBoundaries_.y - ObjectHeight_);
+        transform.position = ViewPos_;
+    }
+}
+ 

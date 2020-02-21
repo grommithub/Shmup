1d7a7b709e446b07039abb1b78cbd0076cc5899d
ROB1N2401 <60138872+ROB1N2401@users.noreply.github.com>
2020-02-21 14:05:54 +0100

    Rough fundament of system is created
    -Added some placeholder assets for test
    -Added some prefabs
    -Added "Abilities" folder in "Scripts"
    -Added "Ability.cs"
    -Added "AbilityCooldown.cs"
    -Added "ShieldAbility.cs"
    -Added "ShieldTrigger.cs"
    -Added a README file, trying to explain how does the system work

---
+++ Shmop/Assets/Scripts/Abilities/ShieldAbility.cs
@@ -0,0 +1,24 @@
+﻿//responsible for the script: Ivan
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Abilities/Shield")] //creating an option to create such asset in unity menu
+public class ShieldAbility : Ability
+{
+    public float shieldDuration_; 
+
+    private ShieldTrigger shield_; //a variable that is going to store a trigger script as a component
+
+    public override void Initialize(GameObject obj_in)
+    {
+        shield_ = obj_in.GetComponent<ShieldTrigger>(); //getting reference to trigger script
+
+        shield_.shieldDuration_ = this.shieldDuration_; //assigning 
+    }
+
+    public override void TriggerAbility()
+    {
+        shield_.DevilTrigger(); //calling trigger function from trigger script
+    }
+}

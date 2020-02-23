e9aa29dd5e9e7be8d63f3dabaf6d16cdc7c35a36
grommithub <44238640+grommithub@users.noreply.github.com>
2020-02-23 20:03:27 +0100

    shieldz

--- Shmop/Assets/Scripts/Abilities/ShieldTrigger.cs
+++ Shmop/Assets/Scripts/Abilities/ShieldTrigger.cs
@@ -6,13 +6,15 @@ using UnityEngine;
 public class ShieldTrigger : MonoBehaviour
 {
     private BoxCollider2D bc2D_;
+    [SerializeField] private GameObject sprite;
 
     [HideInInspector] public float shieldDuration_; //the variables in trigger scripts should be hidden because the values for them are going to be set in the asset
 
     public void DevilTrigger()
     {
         bc2D_ = GetComponent<BoxCollider2D>();
-        bc2D_.enabled = false; 
+        bc2D_.enabled = false;
+        sprite.SetActive(true);
         StartCoroutine(StartCountdown()); //Coroutine allows us to set a desired amount of time for ability to work
     }
 
@@ -20,6 +22,7 @@ public class ShieldTrigger : MonoBehaviour
     {
         yield return new WaitForSeconds(shieldDuration_);
         bc2D_.enabled = true;
+        sprite.SetActive(false);
     }
 }
 

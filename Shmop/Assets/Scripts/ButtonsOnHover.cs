c8dba7248ce2a7af9e9ddda0471b02331b440444
ROB1N2401 <60138872+ROB1N2401@users.noreply.github.com>
2020-03-02 19:19:15 +0100

    Main menu update
    -Added background to the main menu
    -Changed the font of main menu buttons
    -Added "Buttons on hover" script

---
+++ Shmop/Assets/Scripts/ButtonsOnHover.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
+
+public class ButtonsOnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public GameObject textHolder_; //put the child object of the button (text) here
+    private TextMeshProUGUI text_;
+
+    private void Start()
+    {
+        text_ = textHolder_.GetComponent<TextMeshProUGUI>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        text_.color = Color.red;
+        text_.fontMaterial.SetColor(ShaderUtilities.ID_OutlineColor, Color.white);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        text_.color = Color.white;
+        text_.fontMaterial.SetColor(ShaderUtilities.ID_OutlineColor, Color.black);
+    }
+}

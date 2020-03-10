b259e46cc636fec7d41b3a29ab6411c2fe40cff4
grommithub <44238640+grommithub@users.noreply.github.com>
2020-03-10 03:20:17 +0100

    lazer

--- Shmop/Assets/Scripts/Weapons/Weapon.cs
+++ Shmop/Assets/Scripts/Weapons/Weapon.cs
@@ -6,10 +6,12 @@ public class Weapon : MonoBehaviour
 {
     public float _shootSpeed;
     public GameObject projectile;
-    
+
+    internal bool canShoot = true;
 
     public virtual void Shoot(Vector2 position, Quaternion rotation, bool enemy, bool goingRight)
     {
+        if (!canShoot) return;
         GameObject proj = Instantiate(projectile, position, Quaternion.identity);
         proj.transform.up = Quaternion.AngleAxis(-90f + rotation.eulerAngles.z, Vector3.forward) * Vector3.up;
 
